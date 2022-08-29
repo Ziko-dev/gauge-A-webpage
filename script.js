d11462d97353823e4203ab0c0e7007acafdd9fcd
ziko dev <chukwumanwadiogbu100@gmail.com>
2022-08-29 17:19:16 +0100

    new additions

---
+++ script.js
@@ -0,0 +1,12 @@
+let buttonCont = document.getElementById("button-cont")
+var brn = document.getElementById("brn");
+
+function register(){
+brn.style.left = "65%";
+buttonCont.style.backgroundColor ="#400b70";
+}
+    
+function logIn(){
+brn.style.left = "4px"
+buttonCont.style.backgroundColor ="lightgrey";
+}
